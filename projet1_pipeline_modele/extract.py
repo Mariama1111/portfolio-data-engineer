0a300b43e79208af1cac2f2fccbfeee41cac9ebc
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-08 12:27:18 +0100

    Update extract.py

--- projet1_pipeline_modele/extract.py
+++ projet1_pipeline_modele/extract.py
@@ -1,1 +1,13 @@
+""" import de la librairie pandas, téléchargement du dataset public BostonHousing et enregistrement dans le dossier /data/raw..."""
 
+import pandas as pd
+
+def load_raw_data():
+  url = "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+  df = pd.read_csv(url)
+  return df
+
+if __name__ == "__main__":
+    df = load_raw_data()
+    df.to_csv("../data/raw/housing.csv", index=False)
+    print("Données brutes téléchargées et sauvegardées !")
