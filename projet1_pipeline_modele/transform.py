b2f1acf70033ecb44f25366799d658bfd07d9e04
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-08 12:40:28 +0100

    Update transform.py

--- projet1_pipeline_modele/transform.py
+++ projet1_pipeline_modele/transform.py
@@ -1,1 +1,21 @@
+""" Script de suppression de lignes vides, renomme la colonne cible, normalise et sauvegarde des données transfomées"""
 
+import pandas as pd
+
+def transform_data(df):
+  # Supprimer les lignes avec valeurs manquantes
+  df = df.dropna()
+  
+  # renommer une colonne
+  df = df.rename(columns={"medv" : "target"})
+  
+  # exemple de transformation simple : normaliser la colonne target
+  df["target"] = (df["target"].mean()) / df["target"].std()
+  
+  return df
+
+if __name__ == "__main__":
+    df = pd.read_csv("../data/raw/housing.csv")
+    df_clean = transform_data(df)
+    df_clean.to_csv("../data/processed/housing_clean.csv", index=False)
+    print("Données transformées et sauvegardées !")
