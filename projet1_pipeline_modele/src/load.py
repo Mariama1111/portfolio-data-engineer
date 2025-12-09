942d2165de9e4c4b3821f6fcffe2a372826c263d
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 12:45:18 +0100

    Update load.py

--- projet1_pipeline_modele/src/load.py
+++ projet1_pipeline_modele/src/load.py
@@ -1,1 +1,17 @@
+""" Charger le modèle et produire un fichier de prédiction"""
 
+import pandas as pd
+import joblib
+
+def generate_predictions():
+    df = pd.read_csv("../data/processed/data_clean.csv")
+    model = joblib.load("../models/model.pkl")
+
+    X = df.drop("target", axis=1)
+    df["prediction"] = model.predict(X)
+
+    df.to_csv("../data/final/predictions.csv", index=False)
+    print("Prédictions générées dans data/final/predictions.csv")
+
+if __name__ == "__main__":
+    generate_predictions()
