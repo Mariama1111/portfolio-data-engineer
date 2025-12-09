2c7f74288b3435bb25553e9406e0628c37264c0b
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 12:49:00 +0100

    Create validate.py

---
+++ projet1_pipeline_modele/src/validate.py
@@ -0,0 +1,19 @@
+""" Vérification de l'efficacité du modèle"""
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import joblib
+
+def validate_model():
+    df = pd.read_csv("../data/processed/data_clean.csv")
+    model = joblib.load("../models/model.pkl")
+
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    preds = model.predict(X)
+    accuracy = accuracy_score(y, preds)
+    print("Accuracy :", accuracy)
+
+if __name__ == "__main__":
+    validate_model()
