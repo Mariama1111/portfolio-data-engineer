c9e5f1b6f13cfa4ba9c41751540c71825449dd40
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 12:42:52 +0100

    Update train_model.py

--- projet1_pipeline_modele/src/train_model.py
+++ projet1_pipeline_modele/src/train_model.py
@@ -1,1 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+def train_model():
+    df = pd.read_csv("../data/processed/data_clean.csv")
+
+    # selection 
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    # séparation jeu d'entraînement et jeu de test  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # création du modèle
+    model = LogisticRegression()
+  
+    # entrainement du modèle
+    model.fit(X_train, y_train)
+
+    # sauvegarde du modèle .pkl proprement avec joblib
+    joblib.dump(model, "../models/model.pkl")
+    print("Modèle entraîné et sauvegardé !")
+
+if __name__ == "__main__":
+    train_model()
 
