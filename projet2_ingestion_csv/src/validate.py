c2179bf8c30d4150c0cc4e71ada2d343aa8a56a9
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 12:50:48 +0100

    Update validate.py

--- projet2_ingestion_csv/src/validate.py
+++ projet2_ingestion_csv/src/validate.py
@@ -1,1 +1,22 @@
+""" vérifier les colonnes / vérifier si il y a des valeurs manquantes / voir les statistiques """
+
+import pandas as pd
+
+def validate_data():
+    df = pd.read_csv("../data/processed/sales_clean.csv")
+
+    print("Aperçu des données :")
+    print(df.head())
+
+    print("\nColonnes :")
+    print(df.columns)
+
+    print("\nValeurs manquantes :")
+    print(df.isna().sum())
+
+    print("\nStatistiques :")
+    print(df.describe())
+
+if __name__ == "__main__":
+    validate_data()
 
