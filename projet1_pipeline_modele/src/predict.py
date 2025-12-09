b19dff4502ddb7bc48934340d5055f31427ebbad
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 14:04:30 +0100

    Update predict.py

--- projet1_pipeline_modele/src/predict.py
+++ projet1_pipeline_modele/src/predict.py
@@ -9,5 +9,5 @@ def predict(new_data):
 
 if __name__ == "__main__":
    example = {"crim":0.1, "zn":18.0, "indus":2.3, "chas":0, "nox":0.4, "rm":6.0,  "age":65.0, "dis":4.0, "rad":1, "tax":300, "ptratio":15, "b":390, "lstat":5}
-    pred = predict(example)
-    print(f"Prédiction pour l'exemple : {pred:.3f}")
+   pred = predict(example)
+   print(f"Prédiction pour l'exemple : {pred:.3f}")
