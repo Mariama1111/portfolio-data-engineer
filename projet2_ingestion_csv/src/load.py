b45dfc1a28c3c5f8a08dd8e254bf50dbf240bba2
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 10:32:57 +0100

    Update load.py

--- projet2_ingestion_csv/src/load.py
+++ projet2_ingestion_csv/src/load.py
@@ -1,1 +1,16 @@
+""" Script: charge le fichier sales_clean, crée le fichier final, ajoute une petite info (= unité vendue moyenne par employé)"""
 
+import pandas as pd
+
+def load_final_data():
+    df = pd.read_csv("../data/processed/sales_clean.csv")
+
+    # Exemple de chargement : ajouter une statistique
+    avg_units = df["Units Sold"].mean()
+    df["avg_units_sold_global"] = avg_units
+
+    df.to_csv("../data/final/final_sales.csv", index=False)
+    print("Chargement terminé, fichier final créé !")
+
+if __name__ == "__main__":
+    load_final_data()
