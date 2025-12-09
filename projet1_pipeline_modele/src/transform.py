169d30c44f47032e0a9c549e9536b7b205b5898f
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 13:58:37 +0100

    Update transform.py

--- projet1_pipeline_modele/src/transform.py
+++ projet1_pipeline_modele/src/transform.py
@@ -1,21 +1,24 @@
-""" Script de suppression de lignes vides, renomme la colonne cible, normalise et sauvegarde des données transfomées"""
+""" Script de suppression de lignes vides, renomme la colonne cible, sauvegarde des données transfomées"""
 
 import pandas as pd
 
-def transform_data(df):
-  # Supprimer les lignes avec valeurs manquantes
-  df = df.dropna()
-  
-  # renommer une colonne
-  df = df.rename(columns={"medv" : "target"})
-  
-  # exemple de transformation simple : normaliser la colonne target
-  df["target"] = (df["target"].mean()) / df["target"].std()
-  
-  return df
+def transform_data(input_path="../data/raw/housing.csv",
+                   output_path="../data/processed/housing_clean.csv"):
+    df = pd.read_csv(input_path)
+    
+    # Supprimer les lignes avec NaN
+    df = df.dropna()
+    
+    # Renommer la colonne cible
+    df = df.rename(columns={'medv': 'target'})
+    
+    # Convertir toutes les colonnes en float
+    for col in df.columns:
+        df[col] = df[col].astype(float)
+    
+    df.to_csv(output_path, index=False)
+    print(f"Données transformées sauvegardées : {output_path}")
+    return df
 
 if __name__ == "__main__":
-    df = pd.read_csv("../data/raw/housing.csv")
-    df_clean = transform_data(df)
-    df_clean.to_csv("../data/processed/housing_clean.csv", index=False)
-    print("Données transformées et sauvegardées !")
+    transform_data()
