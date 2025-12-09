a0e29d25b5af8e1a206483e5c008e7ea0d978777
Mariama Pasco <126725707+Mariama1111@users.noreply.github.com>
2025-12-09 12:44:31 +0100

    Update predict.py

--- projet1_pipeline_modele/src/predict.py
+++ projet1_pipeline_modele/src/predict.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import joblib
 
+def predict(new_data):
+    model = joblib.load("../models/model.pkl")
+    df = pd.DataFrame([new_data])
+    prediction = model.predict(df)
+    return prediction[0]
+
+if __name__ == "__main__":
+    example = {
+        "feature1": 10,
+        "feature2": 4,
+        "feature3": 20
+    }
+    print("Prédiction :", predict(example))
